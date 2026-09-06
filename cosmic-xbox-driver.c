b691e1999e29c70be2ec2c46026352572ea8fb1b
SawanTeja <tejashvisinha52@gmail.com>
2026-09-06 17:20:45 +0530

    feat(uinput): add virtual Microsoft Xbox 360 pad creation via uinput

--- cosmic-xbox-driver.c
+++ cosmic-xbox-driver.c
@@ -1,5 +1,5 @@
 /*
- * cosmic-xbox-driver.c - Added device detection and EVIOCGRAB
+ * cosmic-xbox-driver.c - uinput virtual Xbox 360 pad setup
  */
 #define _GNU_SOURCE
 #include <stdio.h>
@@ -8,15 +8,31 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <signal.h>
 #include <sys/ioctl.h>
 #include <linux/input.h>
+#include <linux/uinput.h>
 
-static int grab_device(int fd) {
-    return ioctl(fd, EVIOCGRAB, 1);
+static int create_uinput_xbox360(void) {
+    int fd = open("/dev/uinput", O_RDWR | O_NONBLOCK);
+    if (fd < 0) return -1;
+    ioctl(fd, UI_SET_EVBIT, EV_KEY);
+    ioctl(fd, UI_SET_KEYBIT, BTN_A);
+    ioctl(fd, UI_SET_KEYBIT, BTN_B);
+    ioctl(fd, UI_SET_KEYBIT, BTN_X);
+    ioctl(fd, UI_SET_KEYBIT, BTN_Y);
+    struct uinput_setup usetup;
+    memset(&usetup, 0, sizeof(usetup));
+    snprintf(usetup.name, UINPUT_MAX_NAME_SIZE, "Microsoft X-Box 360 pad");
+    usetup.id.bustype = BUS_USB;
+    usetup.id.vendor = 0x045e;
+    usetup.id.product = 0x028e;
+    ioctl(fd, UI_DEV_SETUP, &usetup);
+    ioctl(fd, UI_DEV_CREATE);
+    return fd;
 }
 
 int main(void) {
-    printf("Driver: hardware grabbing with EVIOCGRAB implemented\n");
+    int ufd = create_uinput_xbox360();
+    if (ufd >= 0) close(ufd);
     return 0;
 }
