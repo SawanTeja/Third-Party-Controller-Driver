9db71e4e5d98459ed1217aecf3b3e0c23f39ca14
SawanTeja <tejashvisinha52@gmail.com>
2026-09-08 19:35:10 +0530

    feat(mapping): add ABXY and digital button event translation

--- cosmic-xbox-driver.c
+++ cosmic-xbox-driver.c
@@ -1,5 +1,5 @@
 /*
- * cosmic-xbox-driver.c - uinput virtual Xbox 360 pad setup
+ * cosmic-xbox-driver.c - Button mappings
  */
 #define _GNU_SOURCE
 #include <stdio.h>
@@ -12,27 +12,11 @@
 #include <linux/input.h>
 #include <linux/uinput.h>
 
-static int create_uinput_xbox360(void) {
-    int fd = open("/dev/uinput", O_RDWR | O_NONBLOCK);
-    if (fd < 0) return -1;
-    ioctl(fd, UI_SET_EVBIT, EV_KEY);
-    ioctl(fd, UI_SET_KEYBIT, BTN_A);
-    ioctl(fd, UI_SET_KEYBIT, BTN_B);
-    ioctl(fd, UI_SET_KEYBIT, BTN_X);
-    ioctl(fd, UI_SET_KEYBIT, BTN_Y);
-    struct uinput_setup usetup;
-    memset(&usetup, 0, sizeof(usetup));
-    snprintf(usetup.name, UINPUT_MAX_NAME_SIZE, "Microsoft X-Box 360 pad");
-    usetup.id.bustype = BUS_USB;
-    usetup.id.vendor = 0x045e;
-    usetup.id.product = 0x028e;
-    ioctl(fd, UI_DEV_SETUP, &usetup);
-    ioctl(fd, UI_DEV_CREATE);
-    return fd;
-}
-
-int main(void) {
-    int ufd = create_uinput_xbox360();
-    if (ufd >= 0) close(ufd);
-    return 0;
+static inline void emit_event(int uinput_fd, uint16_t type, uint16_t code, int32_t val) {
+    struct input_event ev;
+    memset(&ev, 0, sizeof(ev));
+    ev.type = type;
+    ev.code = code;
+    ev.value = val;
+    write(uinput_fd, &ev, sizeof(ev));
 }
